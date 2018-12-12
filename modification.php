0ff2a358fc8477f95ec3a2dc14f9a0eadd6705ae
Mario <mariofadegnon@gmail.com>
2018-12-12 11:35:11 +0000

    Update de Post["console"]et Post["possesseur"]

--- modification.php
+++ modification.php
@@ -23,12 +23,7 @@ catch(Exception $e) {
     <title>Mise à jour des données dans la BD</title>
 </head>
 <body>
-   <form action="modification.php" method="post">
-            <label for="newpossesseur">Nouveau Proprietaire de la console : </label> <input type="text" name="possesseur" id="possesseur" value="<?= $sqlSelectData[0]["possesseur"] ?>">
-        <label for="console">Nom de la console : </label> <input type="text" name="console" id="console" value="<?= $sqlSelectData[0]["nom"] ?>">
-        <input type="hidden" name="id" value="<?= $sqlSelectData[0]["ID"] ?>">
-        <input type="submit" value="Valider">
-    </form>
-    <?php $sqlUpdateData = $bdd ->query('UPDATE jeux_video SET possesseur = $_POST["newpossesseur"] WHERE ID= $_POST["id"]'); ?>
+    <?php $sqlUpdateData = $bdd ->query('UPDATE jeux_video SET possesseur = $_POST["possesseur"] WHERE ID= $_POST["id"]'); ?>
+    <?php echo 'Le nom du nouveau possesseur de ' . $_POST["console"] . ' est ' . $_POST["possesseur"] ;?>
 </body>
 </html>
